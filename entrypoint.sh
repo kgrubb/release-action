fade54a7b5828d3cd3c3e464d1ab5cd0e99768dc
kgrubb <keligrubb324@gmail.com>
2019-11-25 13:59:18 -0500

    allow for more than just lists in release descriptions

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,21 +50,18 @@ echo "Version: $VERSION"
 PREVIOUS_VERSION="$(git tag -l | grep -v 'debian' | tail -n1 )"
 
 # get the release notes, remove any empty lines & padded spacing
-RELEASE_NOTE_RAW="$(xmlstarlet sel -t -v '//release[1]' -n data/"$APPDATA"| awk 'NF'| awk '{$1=$1}1')"
+RELEASE_NOTES_RAW="$(xmlstarlet sel -t -m '//release[1]/description/*' -n -c '.' -n data/"$APPDATA" | awk 'NF' | awk '{$1=$1}1')"
 # replace quotes with commented quotes to prevent breakage in github release note string
-RELEASE_NOTES_SANITIZED="${RELEASE_NOTE_RAW//\"/\\\"}"
+RELEASE_NOTES_SANITIZED="${RELEASE_NOTES_RAW//\"/\\\"}"
 echo "Release Note Content:"
 echo -e "$RELEASE_NOTES_SANITIZED\n"
-
 
 #-----------------------#
 # Create Github Release #
 #-----------------------#
 
-# add ul stars to the notes and replace any newlines with a newline character instead
-MARKDOWN_NOTES="$(echo "$RELEASE_NOTES_SANITIZED" | awk '$0="* "$0' | awk '{printf "%s\\n", $0}')"
 # add the project name and version to release note
-GITHUB_RELEASE_NOTE="$PROJECT $VERSION is out! \n\nChanges:\n\n$MARKDOWN_NOTES"
+GITHUB_RELEASE_NOTE="$PROJECT $VERSION is out! \n\nChanges:\n\n$RELEASE_NOTES_SANITIZED"
 DATA="
 {
   \"tag_name\": \"$VERSION\",
@@ -87,13 +84,10 @@ echo -e "\n\033[1;32mA new github release tag has been created!\033[0m\n"
 #-------------------------#
 
 # make sure we are in sync with HEAD
-echo "sync with HEAD"
 git reset --hard HEAD
 
 # get all commit subjects since previous version tag
-echo "start fetching commits"
 COMMITS="$(git log "$PREVIOUS_VERSION"..HEAD --pretty="format:%s")"
-echo "got commits"
 # filter out commits involving translations and commits that don't have a related merge number
 FILTERED_COMMITS="$(echo "$COMMITS" | grep -v 'Weblate' | grep -v 'weblate')"
 
